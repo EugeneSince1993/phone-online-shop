90381391d8b3eed4956868905fe03075438abd3b
EugeneSince1993 <jk235@mail.ru>
2019-11-20 21:55:53 +0300

    ProductList component is completed.

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
 
 // Create Context object
 const ProductContext = React.createContext();
@@ -10,9 +11,27 @@ Provider provides all the information for the our whole application. We can get 
 Whenever we would use the information (that comes from the Provider), we're gonna use the Consumer. */ 
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+  handleDetail = () => {
+    console.log('hello from detail');
+  };
+  addToCart = () => {
+    console.log('hello from add to cart');
+  };
   render () {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider 
+        value={
+          {
+            ...this.state,
+            handleDetail: this.handleDetail,
+            addToCart: this.addToCart
+          }
+        }
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
