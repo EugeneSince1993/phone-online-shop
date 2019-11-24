005051ee0bb641c63f3d6da6906f2c08237863d9
EugeneSince1993 <jk235@mail.ru>
2019-11-24 15:09:32 +0300

    The ProductConsumer component with all the product data is added to the Details component. The cart prop is added to the ButtonContainer styled component.

--- src/context.js
+++ src/context.js
@@ -31,8 +31,8 @@ class ProductProvider extends Component {
   handleDetail = () => {
     console.log('hello from detail');
   };
-  addToCart = () => {
-    console.log('hello from add to cart');
+  addToCart = (id) => {
+    console.log(`cart.id is ${id}`);
   };
   render () {
     return (
