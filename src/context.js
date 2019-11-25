e0398b693b453a6e5f2fc38367a16dddfe42c833
EugeneSince1993 <jk235@mail.ru>
2019-11-25 18:31:47 +0300

    Cart component folder, Cart component and Cartcolumns component are created. 4 Cart methods are added.

--- src/context.js
+++ src/context.js
@@ -16,7 +16,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
   componentDidMount() {
     this.setProducts();
@@ -73,6 +76,18 @@ class ProductProvider extends Component {
       return { modalOpen: false };
     });
   };
+  increment = id => {
+    console.log('this is an increment method');
+  };
+  decrement = id => {
+    console.log('this is a decrement method');
+  };
+  removeItem = id => {
+    console.log('item removed');
+  };
+  clearCart = () => {
+    console.log('cart was cleared');
+  };
   render () {
     return (
       <ProductContext.Provider 
@@ -82,7 +97,11 @@ class ProductProvider extends Component {
             handleDetail: this.handleDetail,
             addToCart: this.addToCart,
             openModal: this.openModal,
-            closeModal: this.closeModal
+            closeModal: this.closeModal,
+            increment: this.increment,
+            decrement: this.decrement,
+            removeItem: this.removeItem,
+            clearCart: this.clearCart
           }
         }
       >
