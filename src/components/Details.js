1a1b8c5a130792cfbd89e08399655f261cb86a2b
EugeneSince1993 <jk235@mail.ru>
2019-11-25 13:24:29 +0300

    The Modal component is done. The openModal and the closeModal methods are added.

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,7 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {
-          (value) => {
+          value => {
             const { id, company, img, info, price, title, inCart } = value.detailProduct;
             return  (
               <div className="container py-5">
@@ -57,6 +57,7 @@ export default class Details extends Component {
                         onClick={
                           () => {
                             value.addToCart(id);
+                            value.openModal(id);
                           }
                         }
                       >
--- src/components/Product.js
+++ src/components/Product.js
@@ -30,6 +30,7 @@ export default class Product extends Component {
                   onClick={
                     () => {
                       value.addToCart(id);
+                      value.openModal(id);
                     }
                   } 
                 >
